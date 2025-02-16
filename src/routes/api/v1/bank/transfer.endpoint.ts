64b127daf844e3e7bb8f73d9680c06b923136f91
xi_pec <xepicscripter@gmail.com>
2025-02-16 11:23:54 +0800

    added admin protected endpoints

---
+++ src/routes/api/v1/bank/transfer.endpoint.ts
@@ -0,0 +1,31 @@
+import { Route, HTTPEndpoint } from "modules/routing/RoutingManager";
+import { Server } from "modules/server/Server"
+import { AuthenticationMiddleware } from "modules/routing/middlewares/auth.middleware";
+
+import { NextFunction, Request, Response } from "express"
+import { z } from "zod";
+
+export default new Route({
+    endpoints: [
+        new HTTPEndpoint({
+            method: "GET",
+            handlers: [
+                AuthenticationMiddleware("HTTP"),
+                async (server: Server, req: Request, res: Response, next: NextFunction) => {
+                    const sessionschema = z.object({ uuid: z.string() })
+                    const session = sessionschema.safeParse(req.session)
+                    if (!session.success) return res.send({ code: 1 })
+                    
+                    const bodyschema = z.object({ uuid: z.string().uuid(), amount: z.number().min(1) })
+                    const body = bodyschema.safeParse(req.body)
+                    if (!body.success) return res.send({ code: 2 })
+
+                    const query = await server.bank.transfer(session.data.uuid, body.data.uuid, body.data.amount)
+                    if (query.code) return res.send({ code: 3 })
+
+                    res.send({ code: 0 })
+                },
+            ]
+        })
+    ]
+});
---
+++ src/routes/api/v1/items/redescribe.endpoint.ts
@@ -0,0 +1,34 @@
+import { Route, HTTPEndpoint } from "modules/routing/RoutingManager";
+import { Server } from "modules/server/Server"
+import { AuthenticationMiddleware } from "modules/routing/middlewares/auth.middleware";
+import { RankMiddleware } from "modules/routing/middlewares/rank.middleware";
+import { Rank } from "modules/managers/users/UserManager";
+
+import { NextFunction, Request, Response } from "express"
+import { z } from "zod";
+
+export default new Route({
+    endpoints: [
+        new HTTPEndpoint({
+            method: "POST",
+            handlers: [
+                AuthenticationMiddleware("HTTP"),
+                RankMiddleware("HTTP", Rank.ADMIN),
+                async (server: Server, req: Request, res: Response, next: NextFunction) => {
+                    const schema = z.object({
+                        uuid: z.string().uuid(),
+                        description: z.string(),
+                    })
+
+                    const { success, data } = await schema.safeParse(req.body)
+                    if (!success) return res.send({ code: 1 })
+                    
+                    const query = await server.items.redescribe(data.uuid, data.description)
+                    if (query.code) return res.send({ code: 2 })
+
+                    res.send({ code: 0 })
+                },
+            ]
+        })
+    ]
+});
