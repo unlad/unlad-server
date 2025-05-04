241e623779c9da2e52c5ce9543b6a6007da24168
xi_pec <xepicscripter@gmail.com>
2025-05-05 02:06:31 +0800

    allow admin to resolve other users

--- src/routes/api/v1/users/resolve.endpoint.ts
+++ src/routes/api/v1/users/resolve.endpoint.ts
@@ -1,6 +1,7 @@
 import { Route, HTTPEndpoint } from "modules/routing/Routing";
 import { Server } from "modules/server/Server"
 import { AuthenticationMiddleware } from "modules/routing/middlewares/auth.middleware";
+import { Rank } from "modules/server/users/UserManager";
 
 import { NextFunction, Request, Response } from "express"
 import { z } from "zod"
@@ -12,12 +13,22 @@ export default new Route({
             handlers: [
                 AuthenticationMiddleware("HTTP"),
                 async (server: Server, req: Request, res: Response, next: NextFunction) => {
-                    const schema = z.object({ uuid: z.string().uuid() })
-                    const { success, data } = schema.safeParse(req.session)
-                    if (!success) return res.send({ code: 1 })
+                    const sessionschema = z.object({ uuid: z.string().uuid(), rank: z.number() })
+                    const session = sessionschema.safeParse(req.session)
+                    if (!session.success) return res.send({ code: 1 })
 
-                    const query = await server.users.resolve(data.uuid)
-                    if (query.code) return res.send({ code: 2 })
+                    let uuid = session.data.uuid
+
+                    if (session.data.rank >= Rank.ADMIN) {
+                        const bodyschema = z.object({ uuid: z.optional(z.string().uuid()) })
+                        const body = bodyschema.safeParse(req.query)
+                        if (!body.success) return res.send({ code: 2 })
+                        
+                        if (body.data.uuid) uuid = body.data.uuid
+                    }
+
+                    const query = await server.users.resolve(uuid)
+                    if (query.code) return res.send({ code: 3 })
 
                     res.send({ 
                         code: 0,
