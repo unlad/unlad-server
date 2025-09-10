2ac69be7bb6d79f576399b3a3dd5f412ff6128d0
xi_pec <xepicscripter@gmail.com>
2025-09-10 14:32:53 +0800

    fix: dynamically load item data on menu

--- src/modules/server/menu/MenuManager.ts
+++ src/modules/server/menu/MenuManager.ts
@@ -3,35 +3,45 @@ import { Item, ItemManager } from "modules/server/items/ItemManager"
 export class MenuManager {
     manager: ItemManager
 
-    items: Map<string, Item> = new Map<string, Item>()
+    items: string[] = []
 
     add(uuid: string) {
-        const query = this.manager.resolve(uuid)
-        if (query.code) return { code: 1 } as const
+        if (this.items.includes(uuid)) return { code: 1 } as const 
 
-        this.items.set(uuid, query.item)
+        const query = this.manager.resolve(uuid)
+        if (query.code) return { code: 2 } as const
+
+        this.items.push(uuid)
 
         return { code: 0 } as const
     }
 
     remove(uuid: string) {
-        const result = this.items.delete(uuid)
-        if (!result) return { code: 1 } as const
+        const index = this.items.findIndex(id => id == uuid)
+        if (index == -1) return { code: 1 } as const
+
+        this.items.splice(index, 1)
         
         return { code: 0 } as const
     }
 
     clear() {
-        this.items.clear()
+        this.items.splice(0)
 
         return { code: 0 } as const
     }
 
     resolve() {
-        return {
-            code: 0,
-            menu: Array.from(this.items.values())
-        } as const
+        const menu = this.items.map(uuid => {
+            const query = this.manager.resolve(uuid)
+            if (query.code) return null
+
+            return query.item
+        })
+
+        if (menu.includes(null)) return { code: 1 }
+
+        return { code: 0, menu } as const
     }
 
     constructor(manager: ItemManager) {
